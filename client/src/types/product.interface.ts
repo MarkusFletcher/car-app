80b4c694d94c39f95b5e4540030c53e10a391475
MarkusFletcher <cehatopfatalist@gmail.com>
2023-05-07 23:59:31 +0300

    add error type, key/keyof

--- client/src/types/product.interface.ts
+++ client/src/types/product.interface.ts
@@ -7,6 +7,9 @@ export interface IProduct {
   imgUrl: string
 }
 
-export interface IProductData extends Omit<IProduct, 'id'> {
-  
-}
+export interface IProductData extends Omit<IProduct, 'id'> {}
+
+export interface IProductDataErrors extends Partial<{[key in keyof IProductData]: string}> {}
+// interface IProductDataErrors extends IProductData {
+//   [key: keyof IProductData]: string;
+// }
