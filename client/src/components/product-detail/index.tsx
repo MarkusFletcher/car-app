0d3fd15e60b5ca0cd6ebcb6a3149de9f4700b236
MarkusFletcher <cehatopfatalist@gmail.com>
2023-05-07 23:58:10 +0300

    change component detail

--- client/src/components/product-detail/index.tsx
+++ client/src/components/product-detail/index.tsx
@@ -10,15 +10,19 @@ type Props = {
 
 export const ProductDetail: React.FC<Props> = ({ product }): ReactElement => {
   return (
-    product ? <div className={style.card}>
+    product ? 
+    <div className={style.detail}>
       <Link className={style.back + ' btn'} to='/catalog'>Back</Link>
-      <div className={style.top}>
-        <img src={product.imgUrl} alt="" />
-      </div>
-      <div className={style.info}>
-        <div className={style.brand}>{ product.brand }</div>
-        <span className={style.model}>{ product.model } </span>
-        <span className={style.year}>{ product.year }</span>
+      <div className={style.content}>
+        <div className={style.media}>
+          <img src={product.imgUrl} alt="" />
+        </div>
+        <div className={style.info}>
+          <div className={style.brand}>{ product.brand }</div>
+          <span className={style.model}>{ product.model } </span>
+          <span className={style.year}>{ product.year }</span>
+          <div className={style.description}>{ product.description }</div>
+        </div>
       </div>
     </div> 
     : <div>...loading</div>
